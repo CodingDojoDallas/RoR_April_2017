40817d2490b0fb3cf82d8ec693db19e2fb43ce62
Erica <ericaesnider@gmail.com>
2017-04-11 21:54:06 -0500

    add all dojo_dashboards, student_dashboard. update survey_form.

--- erica_snider/rails/survey_form/app/controllers/surveys_controller.rb
+++ erica_snider/rails/survey_form/app/controllers/surveys_controller.rb
@@ -4,24 +4,25 @@ class SurveysController < ApplicationController
   end
 
   def create
-    @survey = Survey.new(survey_params)
+    survey = Survey.new(survey_params)
+    # survey = Survey.new(survey_params) # is better because we don't want to send this variable to the view
 
-    if @survey.save
-      session[:survey] = @survey
+    if survey.save
       if session.key?(:count)
         session[:count] += 1
       else
         session[:count] = 1
       end
     else
-      flash[:errors] = @survey.errors
-      session[:survey].clear
+      flash[:errors] = survey.errors  # @survey.errors.full_messages
+
     end
 
-    redirect_to '/result'
+    redirect_to '/result'  # redirect errors to '/' and display errors; redirect succes to '/results'
   end
 
   def result
+    @survey = Survey.last  # do this instead of storing the created survey in session
     render :result
   end
 
