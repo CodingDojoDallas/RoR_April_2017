c9e96775bfb82b1c029fe54fea8ccab528a63681
Erica <ericaesnider@gmail.com>
2017-04-13 20:23:54 -0500

    add model_spec. add capybara, incomplete.

---
+++ erica_snider/rails/capybara/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email))
+    if @user.save
+      flash[:notice] = ['User successfully created']
+      redirect_to new_user_path
+    else
+      #errors we need to code later
+    end
+  end
+end
